365f965c9c7cb6ff8d8ab356dd835f35a252f2d9
Daniel Castilho <dan.castilho@gmail.com>
2021-03-10 21:32:57 -0500

    JS - Adding Event LIsteners

---
+++ script.js
@@ -0,0 +1,27 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+    // If current display value is 0, replace it, if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    displayValue + number;
+}
+
+function clearNumber() {
+    calculatorDisplay.textContent = '0';
+}
+
+// Add Event Listenerts for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => sendNumberValue());
+    } else if (inputBtn.classList.contains('clear')) {
+        inputBtn.addEventListener('click', clearNumber);
+    }
+});
